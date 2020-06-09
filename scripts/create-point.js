84bf7689e4a84ce517bbbd5df148596a1eaaa871
Raafa1993 <rafaelajds@gmail.com>
2020-06-09 10:00:36 -0300

    adicionando itens de coleta e envio do formulario

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -40,3 +40,49 @@ function getCities() {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+//Itens de coleta
+//pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+    item.addEventListener('click', handleSelectedItem)
+}
+
+const collectedItems = document.querySelector('input[name=items]')
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle('selected')
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem itens selecionados, se sim pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex(function (item) {
+        const itemFound = item == itemId // retorna true eou false
+        return itemFound
+
+    })
+
+    //se ja estiver selecionado, tirar da selção
+    if (alreadySelected >= 0) {
+        // tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se nao estiver selecionado
+
+        // adicionar a seleção
+        selectedItems.push(itemId)
+    }
+    console.log(selectedItems)
+
+    //atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+}
